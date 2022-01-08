cd114bb096ef2e3d814f4ff2c0ba3b22bd6415c9
Sparrow007 <ankit.steven007@gmail.com>
2022-01-08 13:51:25 +0530

    updating the thread pool for the network call

--- app/src/main/java/com/androidxlab/dependencyinjection/data/network/DefaultRepository.kt
+++ app/src/main/java/com/androidxlab/dependencyinjection/data/network/DefaultRepository.kt
@@ -10,12 +10,13 @@ import com.androidxlab.dependencyinjection.data.Repository
 import com.androidxlab.dependencyinjection.data.Result
 import com.androidxlab.dependencyinjection.data.api.ApiService
 import com.androidxlab.dependencyinjection.data.model.MovieResult
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class DefaultRepository @Inject constructor(private val service: ApiService) : Repository {
 
     override suspend fun fetchPopularMovies(): LiveData<PagingData<MovieResult>> {
-
         return Pager(
             PagingConfig(pageSize = 10, enablePlaceholders = false)
         ){
@@ -23,8 +24,8 @@ class DefaultRepository @Inject constructor(private val service: ApiService) : R
         }.liveData
     }
 
-    override suspend fun fetchLatestMovies(): Result {
-        return try {
+    override suspend fun fetchLatestMovies(): Result = withContext(Dispatchers.IO){
+         try {
             val result = service.fetchLatestMovies()
             Result.SUCCESS(result)
         }catch (exception: Exception) {
